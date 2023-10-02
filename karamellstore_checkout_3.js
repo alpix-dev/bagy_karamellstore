2cf8a86edeec58c54db95d8b4f5c4d40038c5eda
Eduardo Vieira Lemes <edu.vlemes@gmail.com>
2023-10-02 20:18:42 -0300

    Update and rename karamellstore_checkout_2.js to karamellstore_checkout_3.js

---
+++ karamellstore_checkout_3.js
@@ -0,0 +1,79 @@
+
+  let apx_clientZip = null;
+
+if(history.state.current.includes('/carrinho')){
+  (function(history){
+      var pushState = history.pushState;
+      history.pushState = function(state) {
+          if (typeof history.onpushstate == "function") {
+              history.onpushstate({state: state});
+          }
+          callStateChange();
+          return pushState.apply(history, arguments);
+      };
+  })(window.history);
+}
+
+
+  
+if(history.state.current.includes('/checkout')){
+  setTimeout(() => {
+  apxZip();
+}, "4000");
+}
+  
+
+
+function callStateChange(){
+  console.log('chamou no state', history.state.current)
+  setTimeout(() => {
+    console.log(history.state);
+    if(history.state.current.includes('/checkout')){
+      apxZip();
+    }    
+  }, "2000");
+  
+}
+
+function apxZip(){
+  console.log('APXZIP');
+    if ($('[autocomplete="postal-code"]').val().length === 9) {
+      apx_clientZip = parseInt($('[autocomplete="postal-code"]').val().replace('-', ''));
+    }
+
+    $('body').on('keyup','[autocomplete="postal-code"]',function () {
+      console.log('test')
+      if ($('[autocomplete="postal-code"]').val().length === 9) {
+        apx_clientZip = parseInt($('[autocomplete="postal-code"]').val().replace('-', ''));
+      }
+    });
+
+    // Selecionar o elemento que você deseja monitorar
+    const elementoAlvo = document.querySelector('.checkout-shipping > .step');
+
+    // Criar uma função de callback a ser executada quando a classe mudar
+    const callback = function (mutationsList, observer) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+          if (elementoAlvo.classList.contains('-active')) {
+            $('.apx-titlezip').remove();
+            promocoes_zip.forEach(item => {
+              if (apx_clientZip >= item.cep_ini && apx_clientZip <= item.cep_fim) {
+                $(`<label class="apx-titlezip"><b>Aproveite!!</b> Descontos no frete para sua região!</label><p id="apx-linhazip" style="line-height: 24px;display: block;font-size: 12px;margin:15px 0 0 0">${item.msg}</p>`).insertAfter('.checkout-shipping .step-title');
+              }
+              console.log(item);
+            });
+          }
+        }
+      }
+    };
+
+    // Configurar o observador de mutação
+    const observer = new MutationObserver(callback);
+
+    // Configurar as opções de observação (observe mudanças em atributos)
+    const config = { attributes: true };
+
+    // Iniciar a observação no elemento alvo
+    observer.observe(elementoAlvo, config);
+}
